c2eba42db50bcc80b5348d8dbfc9c4db8eebcd1c
austin <austinanthony226@yahoo.com>
2020-02-12 00:50:35 -0500

    Adding part 2 of Flix

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  
+//
+//  Created by Austin Anthony on 2/11/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var posterViewNew: UIImageView!
+}
